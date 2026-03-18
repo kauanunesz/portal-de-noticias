cc094fab3cea19cbc1787f5c20021f284b6b0f35
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2026-03-17 22:09:47 -0300

    innitial commit

---
+++ pages/cadastro.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text"  placeholder="">
+        <input type="text">
+        <input type="text">
+        <input type="text">
+    </form>
+</body>
+</html>
