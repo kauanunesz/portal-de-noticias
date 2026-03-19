33fe1dde857c8f5ec5cebcebd8e5f67ec26a4bdb
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2026-03-19 20:44:03 -0300

    .

---
+++ php/conexao.php
@@ -0,0 +1,14 @@
+<?php 
+    $server = "localhost";
+    $user = "root";
+    $password = "root";
+    $database = "bd_estoque";
+
+    $conexao = new mysqli($server, $user, $password, $database);
+
+    if ($conexao)
+    {
+        echo "deu certo!";
+    }
+
+?>
