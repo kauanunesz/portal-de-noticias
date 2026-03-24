7d0415598489a2006f8fb214a695c8c1916cf7e9
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2026-03-24 18:12:43 -0300

    início tela de login

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div class="container">
+        <div class="info">
+            <div class="box">
+                <div class="titulo">
+                    <h1 style="color: white">News</h1>
+                    <h1 style="color: rgb(47, 115, 224);">Wire</h1>
+                </div>
+            </div>
+        </div>
+        <div class="linha-vertical"></div>
+        <div class="form-box"></div>
+    </div>
 </body>
 </html>
--- php/conexao.php
+++ php/conexao.php
@@ -10,5 +10,4 @@
     {
         echo "deu certo!";
     }
-
 ?>
