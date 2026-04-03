228652afcc71398826f2656cf99023cc07072933
Kauã Nunes <kauanunes.dev09@gmail.com>
2026-04-03 15:52:26 -0300

    testes de arrays e et

--- teste.php
+++ teste.php
@@ -1,12 +1,39 @@
 <?php 
-    class Carro{
-        public function __construct(private string $modelo, private int $ano){}
-        public function descricao()
-        {
-            echo $this->modelo . " " . $this->ano;
-        }
-    }
-    $carro = new Carro("Fusca", 1973);
-    $carro->descricao();
+    // class Carro{
+    //     public function __construct(private string $modelo, private int $ano){}
+    //     public function descricao()
+    //     {
+    //         echo $this->modelo . " " . $this->ano;
+    //     }
+    // }
+    // $carro = new Carro("Fusca", 1973);
+    // $carro->descricao();
+
+    // $nomes = ["Ana", "Carlos", "Beatriz", "Jo", "Rafael"];
+    // foreach ($nomes as $nome)
+    //     {
+    //         if ($nome === "Carlos") break;     
+    //         if (strlen($nome) > 4) echo $nome . " ";
+    //     }
+
+//=================
+
+//     $produtos = [
+//     ["nome" => "Arroz", "preco" => 5.50],
+//     ["nome" => "Feijão", "preco" => 8.00],
+//     ["nome" => "Macarrão", "preco" => 3.00],
+//     ["nome" => "Café", "preco" => 15.00],
+//     ["nome" => "Açúcar", "preco" => 4.50],
+// ];
+// $contador = 0;
+// foreach ($produtos as $produto)
+//     {
+//         if ($produto["preco"] < 4) continue; // inclusive, não sei se acertei no continue, lembrei por cagada; Pela definição, somente pula aquele valor?
+//         if ($produto["nome"] === "Café") break;
+//         echo $produto["nome"] . " " . $produto["preco"] . "<br> ";
+//         $contador++;
+//         }
+//         echo "O array percorreu {$contador} produtos";  
+
+
 ?>
-<!--  -->
