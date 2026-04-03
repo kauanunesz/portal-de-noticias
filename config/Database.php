8f2b3a082a9173e742f66bd0d2d9db8afc48473f
Kauã Nunes <kauanunes.dev09@gmail.com>
2026-04-03 00:55:25 -0300

    /

--- config/Database.php
+++ config/Database.php
@@ -11,5 +11,4 @@ try {
 catch (PDOException $e) {
     echo "Erro: ". $e->getMessage();
 }
-
 ?>
---
+++ teste.php
@@ -0,0 +1,11 @@
+<?php 
+    class Carro{
+        public function __construct(private string $modelo, private int $ano){}
+        public function descricao()
+        {
+            echo $this->modelo . " " . $this->ano;
+        }
+    }
+    $carro = new Carro("Fusca", 1973);
+    $carro->descricao();
+?>
