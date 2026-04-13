d8095d9435617fef825742471d068ee2cd04710a
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2026-04-13 15:56:00 -0300

    melhorando lógica de roteamento

--- index.php
+++ index.php
@@ -1,15 +1,30 @@
 <?php 
+    session_start();
     require_once './config/Database.php';
     require_once './src/models/ModelUsuario.php';
     require_once './src/controllers/AuthController.php';
 
-    $pagina = $_GET['pagina'];
-    switch ($pagina)
-    {
-        case 'login': 
-            header("Location: ./src/views/auth/login.php");
-            break;
-        // case 'cadastro':
-            
-    }
+     $pagina = $_GET['pagina' ?? ''];
+switch ($pagina)
+{
+    case 'login': 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST')
+        {
+            try {
+                $controller = new AuthController();
+                $controller->login();
+                
+            } catch (\Throwable $e) {
+                $e->getMessage();
+            }
+        }
+        else
+        {
+            include './src/views/auth/login.php';
+        }
+        break;
+    case 'cadastro':
+
+        
+}
 ?>
